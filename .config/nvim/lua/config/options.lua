b4be202ae61b9b26bffede54c74401d2764657ff
Fabián Sanhueza <sanhuesoft@gmail.com>
2026-06-17 16:55:32 -0400

    options y stylua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,4 @@ vim.opt.wrap = true -- Activa el salto de línea visual
 vim.opt.breakindent = true -- Hace que la línea que "cae" mantenga el indentado
 vim.opt.linebreak = true -- Evita que las palabras se corten por la mitad
 vim.loader.enable()
+
